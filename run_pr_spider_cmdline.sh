55e589b2283454cb761653b36bd5a78a5b2595da
Kerne Fahey <kernefahey@protonmail.com>
2024-02-17 13:23:56 -0800

    added environment validation checks to run_pr_spider_cmdline.sh

--- run_pr_spider_cmdline.sh
+++ run_pr_spider_cmdline.sh
@@ -1,4 +1,10 @@
-#!/bin/sh -x
+#!/usr/bin/bash -x
+
+# Check if .env file exists
+if [ ! -f ".env" ]; then
+    echo "Error: .env file not found in the current directory."
+    exit 1
+fi
 
 # Check if the number of arguments is either 3 or 4
 if [ "$#" -lt 3 ] || [ "$#" -gt 4 ]; then
@@ -14,7 +20,7 @@ fi
 
 # Validate second and third arguments are integers and second is less than third
 if ! [ "$2" -eq "$2" ] 2> /dev/null || ! [ "$3" -eq "$3" ] 2> /dev/null || ! [ "$2" -le "$3" ]; then
-    echo "Second and third arguments must be integers, and the second must be less than or equal toor equal to the third"
+    echo "Second and third arguments must be integers, and the second must be less than or equal to the third"
     exit 1
 fi
 
@@ -24,8 +30,18 @@ if [ -n "$4" ] && ! [ -d "$4" ]; then
     exit 1
 fi
 
-export HTTP_PROXY="http://scraperapi:6056b5b46c5d1648afe91e266c1f7988@proxy-server.scraperapi.com:8001/"
-export HTTPS_PROXY="http://scraperapi:6056b5b46c5d1648afe91e266c1f7988@proxy-server.scraperapi.com:8001/"
+set -a # automatically export all variables
+source .env
+set +a # stop automatically exporting
+
+# Check if SCR_API_KEY variable is defined
+if [ -z "$SCR_API_KEY" ]; then
+    echo "Error: SCR_API_KEY variable not defined. Please define it in the .env file."
+    exit 1
+fi
+
+export HTTP_PROXY="http://scraperapi:${SCR_API_KEY}@proxy-server.scraperapi.com:8001/"
+export HTTPS_PROXY="http://scraperapi:${SCR_API_KEY}@proxy-server.scraperapi.com:8001/"
 
 # Conditional command execution based on the presence of the fourth argument
 if [ -n "$4" ]; then
@@ -33,4 +49,3 @@ if [ -n "$4" ]; then
 else
     scrapy crawl propertyrecords -a "start_with_tacoma=$1" -a "credits_used=$2" -a "credits_threshold=$3" 2>&1 | tee -a countyoffice_spider.log
 fi
-
